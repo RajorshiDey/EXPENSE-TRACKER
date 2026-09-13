5042effcfee4a0d75a54ab39ff10a1509998712e
RajorshiDey <rajorshideys@gmail.com>
2026-09-13 23:26:08 +0530

    Added all the CRUD Operations

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5500",
+    origin: ["http://localhost:5500", "https://exxpense-trracker.netlify.app/"],
     credentials: true
 }));
 
